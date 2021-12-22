544cd46dee8f868ebdaed6ad5bef4a3c1f405c15
Mayro <deehmyller11@gmail.com>
2021-12-22 18:06:44 -0300

    chore: add scss and setting global styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>React Newsletter</title>
+      </Head>
+      <div>
+        <h1>React News</h1>
+      </div>
+    </>
+  )
+}
