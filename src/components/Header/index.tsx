f85e57c38e2b29df7bf4aeb7e2aa9b9b158c3bb2
Mayro <deehmyller11@gmail.com>
2021-12-28 21:07:44 -0300

    chore: create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <img src="/images/logo.svg" alt="React Newsletter" />
+
+        <nav className={styles.navigation}>
+          <a className={styles.navigation__link}>Home</a>
+          <a className={styles.navigation__link}>Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
