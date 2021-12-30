29fe7e4a4730c128105685da09fa3ce7eadcbe10
Mayro <deehmyller11@gmail.com>
2021-12-29 22:32:17 -0300

    chore: update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
 import Head from 'next/head'
 
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>React Newsletter</title>
       </Head>
-      <div>
-        <h1>React News</h1>
-      </div>
+
+      <main className={styles.content}>
+        <section className={styles.hero}>
+          <span className={styles.welcome}>Hey, welcome</span>
+
+          <h1 className={styles.title}>
+            News about the <span>React</span> the world.
+          </h1>
+          <p className={styles.description}>
+            Get access to all the publication <br />
+            <span className={styles.pricing}>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/woman.svg" alt="Woman coding" />
+      </main>
     </>
   )
 }
