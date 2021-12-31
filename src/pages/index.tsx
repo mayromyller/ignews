058be134c0c28f9baa08e790f320db9643307579
Mayro <deehmyller11@gmail.com>
2021-12-31 12:59:31 -0300

    chore: add product value of stripe in SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+
+import { stripe } from '../services/stripe'
+
 import SubscribeButton from '../components/SubscribeButton'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -19,14 +30,32 @@ export default function Home() {
           </h1>
           <p className={styles.description}>
             Get access to all the publication <br />
-            <span className={styles.pricing}>for $9.90 month</span>
+            <span className={styles.pricing}>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/woman.svg" alt="Woman coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KCX9lIIFgtLy4ClEpKwEPMi')
+
+  const product = {
+    priceId: price.id,
+    amount: Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
