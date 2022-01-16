fdb1dbd722249c47d1630615331d8b5bf4be907d
Mayro <deehmyller11@gmail.com>
2022-01-15 23:32:18 -0300

    feat: auth config in SignInButton

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,19 +1,25 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const userIsLogged = true
+  const { data: session } = useSession()
 
-  return userIsLogged ? (
-    <button className={styles.signIn_button}>
+  return session ? (
+    <button className={styles.signIn_button} onClick={() => signOut()}>
       <FaGithub color="#04D361" />
-      User Name
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signIn_button}>
+    <button
+      type="button"
+      className={styles.signIn_button}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#EBA417" />
       Sing in with GitHub
     </button>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import { AppProps } from 'next/app'
 
-import '../styles/global.scss'
+import { SessionProvider } from 'next-auth/react'
 
 import { Header } from '../components/Header'
 
+import '../styles/global.scss'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 
