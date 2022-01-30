869377c48079d389191dacb1c60640762a96f198
Mayro <deehmyller11@gmail.com>
2022-01-30 17:04:44 -0300

    chore: add callback signIn in next-auth to create or update user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -12,5 +16,28 @@ export default NextAuth({
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
