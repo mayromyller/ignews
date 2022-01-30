d902163c050ab10aa695c25ced7acfe79897ff3e
Mayro <deehmyller11@gmail.com>
2022-01-30 18:03:05 -0300

    chore: create a function to initiate checkout session on stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,9 +1,35 @@
+import { useSession, signIn } from 'next-auth/react'
+
 import styles from './styles.module.scss'
+import { api } from '../../services/axios'
+import { getStripeJs } from '../../services/stripe-js'
 
 interface SubscribeButtonProps {
   priceId: string
 }
 
 export default function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  return <button className={styles.subscribeButton}>Subscribe now</button>
+  const { data: session } = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+      const { sessionId } = response.data
+      const stripeJs = await getStripeJs()
+      await stripeJs.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
+  return (
+    <button className={styles.subscribeButton} onClick={handleSubscribe}>
+      Subscribe now
+    </button>
+  )
 }
